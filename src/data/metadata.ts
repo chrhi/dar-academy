7c7e1633c9d64270e16bae582e730e3698353782
abdellah <Abdellah.chehri14@gmail.com>
2023-11-28 11:02:31 +0100

    push to main

---
+++ src/data/metadata.ts
@@ -0,0 +1,45 @@
+import { Metadata } from "next";
+
+export function constructMetadata({
+  title = "Dar Academy",
+  description = "Dar Academy institus de formations",
+  image = "/banner.jpg",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@KING_IN_JUNGLE",
+    },
+    icons,
+    metadataBase: new URL("https://quill-blcrm7149-chrhi.vercel.app"),
+    themeColor: "#FFF",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
