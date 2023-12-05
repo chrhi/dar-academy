bcddd6d1f33cb48768233e0eb7afd8bd259038c4
abdellah <Abdellah.chehri14@gmail.com>
2023-12-05 13:07:54 +0100

    push to main

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5U-pBgn6g1djdEg1SYSYDPiDQBwsFf6M",
+  authDomain: "dar-academy-6dc13.firebaseapp.com",
+  projectId: "dar-academy-6dc13",
+  storageBucket: "dar-academy-6dc13.appspot.com",
+  messagingSenderId: "361210063513",
+  appId: "1:361210063513:web:560601b30ef3c81671a608",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
