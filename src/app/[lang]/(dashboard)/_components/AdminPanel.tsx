2872e31947fb26ae4727d587cc8da0e7fbbdcdb5
abdellah <Abdellah.chehri14@gmail.com>
2023-12-06 17:10:17 +0100

    push to main

--- src/app/[lang]/(dashboard)/_components/AdminPanel.tsx
+++ src/app/[lang]/(dashboard)/_components/AdminPanel.tsx
@@ -1,9 +1,118 @@
-import type { FC } from "react";
+"use client";
 
-interface AdminPanelAbdullahProps {}
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
-const AdminPanel: FC = ({}) => {
-  return <div>AdminPanel</div>;
-};
+import { Icons } from "@/components/icons";
+import { useRouter, usePathname } from "next/navigation";
+
+interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+function AdminPanel({ className }: SidebarProps) {
+  const router = useRouter();
+
+  const path = usePathname();
+
+  return (
+    <div className={cn("pb-12 bg-slate-950", className)}>
+      <div className="space-y-4 py-4">
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg text-white font-semibold tracking-tight">
+            school managment 🚀
+          </h2>
+          <div className="space-y-1">
+            <Button
+              onClick={() => router.push("/admin")}
+              variant={
+                path === "/dashboard" ? "darkGhostSelected" : "darkGhost"
+              }
+              className="w-full justify-start"
+            >
+              <Icons.calendar className="w-4 h-4 mr-2 " />
+              Home
+            </Button>
+            <Button
+              onClick={() => router.push("/orders")}
+              variant={
+                path === "/dashboard/teachers"
+                  ? "darkGhostSelected"
+                  : "darkGhost"
+              }
+              className="w-full justify-start"
+            >
+              <Icons.calendar className="w-4 h-4 mr-2 " />
+              Teachers
+            </Button>
+            <Button
+              onClick={() => router.push("/customers")}
+              variant={
+                path === "/dashboard/courses"
+                  ? "darkGhostSelected"
+                  : "darkGhost"
+              }
+              className="w-full justify-start"
+            >
+              <Icons.calendar className="w-4 h-4 mr-2 " />
+              Courses
+            </Button>
+            <Button
+              onClick={() => router.push("/customers")}
+              variant={
+                path === "/dashboard/students"
+                  ? "darkGhostSelected"
+                  : "darkGhost"
+              }
+              className="w-full justify-start"
+            >
+              <Icons.calendar className="w-4 h-4 mr-2 " />
+              Students
+            </Button>
+          </div>
+        </div>
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-white text-lg font-semibold tracking-tight">
+            website ✨
+          </h2>
+          <div className="space-y-1">
+            <Button
+              onClick={() => router.push("/products")}
+              variant={
+                path === "/dashboard/blog" ? "darkGhostSelected" : "darkGhost"
+              }
+              className="w-full justify-start"
+            >
+              <Icons.calendar className="w-4 h-4 mr-2 " />
+              Blog
+            </Button>
+            <Button
+              onClick={() => router.push("/editor")}
+              variant={
+                path === "/dashboard/settings"
+                  ? "darkGhostSelected"
+                  : "darkGhost"
+              }
+              className="w-full justify-start"
+            >
+              <Icons.calendar className="w-4 h-4 mr-2 " />
+              Settings
+            </Button>
+            <Button
+              onClick={() => router.push("/analitycs")}
+              variant={
+                path === "/dashboard/analitycs"
+                  ? "darkGhostSelected"
+                  : "darkGhost"
+              }
+              className="w-full justify-start"
+            >
+              <Icons.chart className="w-4 h-4 mr-2 " />
+              Analitycs
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default AdminPanel;
--- src/app/[lang]/(dashboard)/layout.tsx
+++ src/app/[lang]/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import { Locale, i18n } from "@/i18n.config";
 import { Tajawal, Poppins } from "next/font/google";
+import AdminPanel from "./_components/AdminPanel";
+import { Toaster } from "sonner";
 
 export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }));
@@ -13,5 +15,14 @@ export default function DashboardLayout({
   children: React.ReactNode;
   params: { lang: Locale };
 }) {
-  return <div className="min-h-screen w-full bg-red-500">{children}</div>;
+  return (
+    <div className="flex w-full min-h-screen">
+      <AdminPanel className="block w-[250px] fixed left-0  top-0 bottom-0 " />
+
+      <main className="w-[calc(100%-250px)] ml-[250px] bg-gray-50 ">
+        {children}
+      </main>
+      <Toaster />
+    </div>
+  );
 }
