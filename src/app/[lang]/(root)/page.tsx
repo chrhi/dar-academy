81c0c9ae670855b0317154e01cb98a9c94f3cf40
abdellah <Abdellah.chehri14@gmail.com>
2023-12-07 21:01:00 +0100

    push to main

--- src/app/[lang]/(root)/page.tsx
+++ src/app/[lang]/(root)/page.tsx
@@ -20,7 +20,8 @@ export default async function Pape({
 }: {
   params: { lang: Locale };
 }) {
-  const { landing } = await getDictionary(lang);
+  const { landing, services, reviews, reviewsTitle, findus } =
+    await getDictionary(lang);
 
   return (
     <MaxWidthWrapper>
@@ -31,16 +32,14 @@ export default async function Pape({
           className=" relative mx-auto flex w-full max-w-[64rem] flex-col items-center justify-center gap-4 pt-12  text-center md:pt-32"
         >
           <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-            <p className="text-sm font-semibold text-gray-700">
-              Quill is now public!
-            </p>
+            <p className="text-sm font-semibold text-gray-700">{landing.sub}</p>
           </div>
           <Balancer
             as="h1"
             className="font-heading font-bold text-5xl md:text-6xl lg:text-7xl"
           >
             <span className="text-blue-500">{landing.name}</span> <br />
-            ecol de formation
+            {landing.follow}
           </Balancer>
           <Balancer className="max-w-[42rem] font-semibold leading-normal text-muted-foreground sm:text-xl sm:leading-8">
             {landing.description}
@@ -68,27 +67,31 @@ export default async function Pape({
             />
           </div>
           <h2 className="text-sm  font-semibold text-gray-700 text-center my-4 ">
-            Our Services
+            {services["Our Services"]}
           </h2>
           <div className="flex min-w-full w-fit mx-auto justify-center items-start gap-4  flex-wrap">
             <div className="w-[100px] h-[100px] bg-blue-500 rounded-lg shadow flex items-center justify-center">
               <span className="text-md text-white font-semibold text-center">
-                Les Course Pour Primaire
+                {/* @ts-ignore */}
+                {services["Primary School Courses"]}
               </span>
             </div>
             <div className="w-[100px] h-[100px] bg-yellow-400 rounded-lg shadow flex items-center justify-center">
               <span className="text-md text-white font-semibold text-center">
-                Les Course Pour Collège
+                {/* @ts-ignore */}
+                {services["Middle School Courses"]}
               </span>
             </div>
             <div className="w-[100px] h-[100px] bg-sky-500 rounded-lg shadow flex items-center justify-center">
               <span className="text-md text-white font-semibold text-center">
-                Les Course Pour Lycée
+                {/* @ts-ignore */}
+                {services["High School Courses"]}
               </span>
             </div>
             <div className="w-[100px] h-[100px] bg-violet-500 rounded-lg shadow flex items-center justify-center">
               <span className="text-md text-white font-semibold text-center">
-                Les formation pratique
+                {/* @ts-ignore */}
+                {services["Practical Training Courses"]}
               </span>
             </div>
           </div>
@@ -97,12 +100,11 @@ export default async function Pape({
         <section className=" w-full md:w-[800px] mx-auto  h-fit my-8">
           <div className="w-full min-h-[500px] h-fit flex flex-col lg:flex-row items-center justify-center  md:gap-x-12 lg:gap-x-24">
             <div className="w-[400px] h-full flex p-6 lg:p-0 flex-col items-start justify-center gap-y-4">
-              <h2 className="text-2xl font-bold ">Who are we ?</h2>
+              <h2 className="text-2xl font-bold ">
+                {landing["who are we "].title}
+              </h2>
               <p className="text-md font-gray-700 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-                blanditiis odio consequuntur quaerat nihil, porro, pariatur
-                quia, iure illo assumenda quasi. Neque velit iste aut a minima
-                vel, provident ex!
+                {landing["who are we "].description}
               </p>
             </div>
             <div className="w-[400px] relative h-full  flex flex-col items-center justify-center gap-y-4">
@@ -128,23 +130,21 @@ export default async function Pape({
               <div className="h-[100px] w-[235px] top-[20px] z-[-20] absolute bg-blue-500  right-[50px] " />
             </div>
             <div className="w-[400px] h-full flex flex-col p-6 lg:p-0 items-start justify-center gap-y-4">
-              <h2 className="text-2xl font-bold ">Our Mession </h2>
+              <h2 className="text-2xl font-bold ">
+                {landing["our mession"].title}{" "}
+              </h2>
               <p className="text-md font-gray-700 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-                blanditiis odio consequuntur quaerat nihil, porro, pariatur
-                quia, iure illo assumenda quasi. Neque velit iste aut a minima
-                vel, provident ex!
+                {landing["our mession"].description}
               </p>
             </div>
           </div>
           <div className="w-full min-h-[500px] h-fit flex items-center justify-center flex-col lg:flex-row  md:gap-x-12 lg:gap-x-24">
             <div className="w-[400px] h-full flex flex-col  p-6 lg:p-0 items-start justify-center gap-y-4">
-              <h2 className="text-2xl font-bold ">Why choose us ?</h2>
+              <h2 className="text-2xl font-bold ">
+                {landing["why choose us"].title}
+              </h2>
               <p className="text-md font-gray-700 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-                blanditiis odio consequuntur quaerat nihil, porro, pariatur
-                quia, iure illo assumenda quasi. Neque velit iste aut a minima
-                vel, provident ex!
+                {landing["why choose us"].description}
               </p>
             </div>
             <div className="w-[400px] h-full  relative flex flex-col items-center justify-center gap-y-4">
@@ -163,24 +163,19 @@ export default async function Pape({
 
         <section id="tesimonials" className="w-full h-fit">
           <h2 className=" text-2xl lg:text-4xl font-bold text-start md:text-center my-12 ">
-            The reviews are in.
+            {reviewsTitle}
           </h2>
           <div className="flex w-full mx-auto max-w-[750px] justify-start items-start gap-6 flex-wrap">
-            <TestimonialCard
-              auther="abdellah chehri"
-              position="studnet"
-              comment="JS Mastery's Next.js course is amazing. The way Adrian structures the course by giving you active lessons that you build out on your own is perfect for learning. These active lessons have just the right amount of difficulty so you are never lost yet are always pushing yourself and learning more. Also, by the end of the course you will have an incredible project that would make any portfolio stand out."
-            />
-            <TestimonialCard
-              auther="abdellah chehri"
-              position="studnet"
-              comment="This course is a game-changer for anyone serious about mastering Next.js 14. Adrian's teaching style is engaging, the curriculum is exhaustive, and the focus on active, hands-on learning is unparalleled. Building DevFlow, a real-world app, can not only solidified your understanding but also gave you a portfolio-worthy project. If you want to become a top 1% Next.js developer, this course is your roadmap."
-            />
-            <TestimonialCard
-              auther="abdellah chehri"
-              position="studnet"
-              comment="JS Mastery's Next.js 14 course is a transformative journey for aspiring top-tier Next.js developers. Adrian's hands-on approach ties every concept to real-world projects, making the learning curve practical and manageable. Even with a good understanding of Next.js, I was lifted to new levels of skill and insight. The course delves deep, touching on advanced architectural decisions and patterns, going beyond the basics. Whether you're a newbie or already familiar with Next.js, this course is an investment in becoming a top 1% Next.js developer. Adrian's engaging teaching style makes complex topics digestible and fun, creating a rewarding learning experience."
-            />
+            {reviews.map((item, index) => {
+              return (
+                <TestimonialCard
+                  key={item.auther.name + index}
+                  auther={item.auther.name}
+                  position="studnet"
+                  comment={item.content}
+                />
+              );
+            })}
           </div>
         </section>
 
@@ -189,7 +184,7 @@ export default async function Pape({
           className="w-full h-fit flex flex-col items-start justify-center lg:items-center"
         >
           <h2 className=" text-2xl lg:text-4xl font-bold text-start md:text-center my-12 ">
-            where you can find us?
+            {findus}
           </h2>
           <Map />
         </section>
