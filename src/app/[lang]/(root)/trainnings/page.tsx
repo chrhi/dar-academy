b3f917273683aa8ce1bff73286e3a1f710d65e3e
abdellah <Abdellah.chehri14@gmail.com>
2023-12-17 10:08:05 +0100

    push to main

--- src/app/[lang]/(root)/trainnings/page.tsx
+++ src/app/[lang]/(root)/trainnings/page.tsx
@@ -8,18 +8,18 @@ const page: FC = ({}) => {
   return (
     <main className="w-full h-fit ">
       <MaxWidthWrapper>
-        <section className="w-full h-[400px] bg-blue-500 border-2 border-black ralative flex items-center justify-center ">
-          <h1 className="text-3xl font-bold text-center text-white">
-            FIND ALL OF OUT TRAINNINGS
+        <section className="w-full h-[400px] bg-gradient-to-r from-blue-500 to-violet-500 border-2 border-black rounded-lg ralative flex items-center justify-center ">
+          <h1 className="text-3xl lg:text-6xl font-bold text-center text-white">
+            RETROUVEZ TOUTES NOS FORMATIONS
           </h1>
         </section>
 
         <section className="my-4 w-full h-fit">
           <h2 className="text-2xl  lg:text-4xl  text-black font-bold text-start">
             {" "}
-            Most Popular Resources
+            Ressources les plus populaires
           </h2>
-          <div className="w-full h-fit min-h-[500px] flex flex-wrap gap-4">
+          <div className="w-full my-4 h-fit min-h-[500px] flex flex-wrap gap-4">
             {[1, 2, 3, 4, 5].map((item) => (
               <TrainningCard key={item} />
             ))}
@@ -28,9 +28,9 @@ const page: FC = ({}) => {
         <section className="my-4 w-full h-fit">
           <h2 className="text-2xl  lg:text-4xl  text-black font-bold text-start">
             {" "}
-            Helpful Cheatsheets
+            Ressources utiles
           </h2>
-          <div className="w-full h-fit min-h-[500px] flex flex-wrap gap-4">
+          <div className="w-full my-4 h-fit min-h-[500px] flex flex-wrap gap-4">
             {[1, 2, 3, 4, 5].map((item) => (
               <TrainningCard key={item} />
             ))}
--- src/components/trainning-card.tsx
+++ src/components/trainning-card.tsx
@@ -1,10 +1,25 @@
 import type { FC } from "react";
+import { Button } from "./ui/button";
 
 interface TrainningCardAbdullahProps {}
 
 const TrainningCard: FC = ({}) => {
   return (
-    <div className="w-[300px] h-[500px] bg-orange-500 border-2 border-black"></div>
+    <div className="w-[350px] h-[450px] rounded-lg  border shadow">
+      <div className="w-full h-[200px] bg-black rounded-t-lg"></div>
+      <div className="flex flex-col items-start p-4 pl-2">
+        <h2 className="text-xl font-semibold">Andaroid development</h2>
+        <p className="line-clamp-5 text-gray-700">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil,
+          incidunt sunt obcaecati natus neque consectetur corrupti nemo eos
+          alias facilis unde perferendis iure aut cumque. Voluptatum,
+          doloremque. Necessitatibus, dicta debitis?
+        </p>
+      </div>
+      <div className="w-full h-[50px] flex justify-end items-center p-4 ">
+        <Button>Read more</Button>
+      </div>
+    </div>
   );
 };
 
